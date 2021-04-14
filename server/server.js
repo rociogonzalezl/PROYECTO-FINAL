4baa374ae603cdc011950e2a3ec7437bcafd162d
Rocio.Gonzalez <180707@utags.edu.mx>
2021-04-13 20:52:52 -0500

    Conexion a BD

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
  
@@ -60,7 +61,18 @@ app.delete('/usuarios/:id', function(req, res){
         id: id
     });
 });
+
+ mongoose.connect('mongodb://localhost:27017/starbucks',{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+ }, (err, res) => {
  
+    if(err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
 app.listen(process.env.PORT, ()=>{
     console.log('El servidor esta en linea por el puerto', process.env.PORT);
 }); 
