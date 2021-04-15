ca1212f52197d903371dd040b87d4a1e6851fb7d
Rocio.Gonzalez <180707@utags.edu.mx>
2021-04-15 01:28:33 -0500

    Codigo terminado y corrido en BD

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,52 +1,99 @@
+const { json } = require('body-parser');
+const { request, query } = require('express');
 const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
 const app = express();
-  
-  app.get('/usuarios', function (req, res) {
-      res.json({
-          ok: 200,
-          msj: 'Usuarios consultados exitosamente'
-      });
-    });
-  
-    app.post('/usuarios', function (req, res){
-        let nombre = req.body.nombre;
-        let body =req.body;
-  
-        if(nombre === undefined){
-            res.status(400).json({
-              ok:400,
-              msj: 'Favor de enviar el valo del nombre'
-            });
-        }else{
-        
-          res.json({
-              ok: 200,
-              msj: 'Usuario insertado con exito',
-              body: body
-          });
-      }
-    });
-  
-  app.put('/usuarios/:id/: nombre', function(req, res){
-      let id = req.params.id;
-      let nombre = req.params.nombre;
-  
-      res.json({
-          ok: 200, 
-          msj:'Usuario actualizado exitozamente',
-          id: id,
-          nombre: nombre
-      });
-  });
-  
-  app.delete('/usuarios/:id', function(req, res){
-      let id = req.params.id;
-  
-      res.json({
-          ok: 200,
-          msj: 'Usuario eliminado con exito',
-          id: id
-      });
-  });
 
-  module.exports = app;
+app.get('/usuario', function(req, res) {
+    let desde = req.query.desde || 0;
+    let hasta = req.query.hasta || 5;
+
+    Usuario.find({ estado: true })
+        .skip(Number(desde))
+        .limit(Number(hasta))
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400), json({
+                    ok: false,
+                    msg: 'Ocurrio un error al momento de consultar',
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                msg: 'Lista de usuarios obtenida con exito',
+                conteo: usuarios.length,
+                usuarios
+            });
+        });
+});
+
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    let usr = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password
+        //bcrypt.hashSync(body.password, 10)
+    });
+
+    usr.save((err, usrDB) => {
+        if (err) {
+           return res.status(400).json({
+                ok: false,
+                msg: 'Ocurrio un error',
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            msg: 'Usuario insertado con exito',
+            usrDB
+        });
+    });
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' },
+        (err, usrDB) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ocurrio un error al momento de actualizar',
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                msg: 'Usuario actualizado con exito',
+                usuario: usrDB
+            });
+        });
+});
+
+app.delete('/Usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    Usuario.findByIdAndUpdate(id, { estado: false }, { new: true, runValidators: true, context: 'query' }, (err, usrDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ocurrio un error al momento de eliminar',
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            msg: 'Usuario eliminado con exito',
+            usrDB
+        });
+    });
+});
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -3,31 +3,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
- 
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
- 
+
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send('<h1>Bienvenido a mi servidor REST</h1>');
 });
 
 app.use(require('./routes/usuario'));
+app.use(require('./routes/categoria'));
+app.use(require('./routes/producto'));
 
+mongoose.connect('mongodb://localhost:27017/starbucks', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
 
- mongoose.connect('mongodb://localhost:27017/starbucks',{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
- }, (err, res) => {
- 
-    if(err) throw err;
+}, (err, res) => {
+    if (err) throw err;
     console.log('Base de datos ONLINE');
 });
 
-app.listen(process.env.PORT, ()=>{
-    console.log('El servidor esta en linea por el puerto', process.env.PORT);
-}); 
+
+app.listen(process.env.PORT, () => {
+    console.log('El servidor esta en linea por el puerto ', process.env.PORT);
+});
